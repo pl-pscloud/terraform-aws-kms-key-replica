a39d587d8f9cbdfa91d12ec51a6d7c43445912d4
pswitala <p.switala@pslab.eu>
2022-03-05 13:17:16 +0100

    Changed output vars

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "pscloud_key_replica_id" {
-  value = aws_kms_key.pscloud-kms-key-replica.id
+  value = aws_kms_replica_key.pscloud-kms-key-replica.id
 }
 
 output "pscloud_key_replica_arn" {
-  value = aws_kms_key.pscloud-kms-key-replica.arn
+  value = aws_kms_replica_key.pscloud-kms-key-replica.arn
 }
